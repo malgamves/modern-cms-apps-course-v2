8ab32a117adcf2cefdc20bcf5123ca0a5d5cd708
malgamves <malgamves@gmail.com>
2023-05-04 11:34:43 +0200

    lesson 12

--- modern-cms-client/components/Content.js
+++ modern-cms-client/components/Content.js
@@ -1,8 +1,9 @@
+import ReactMarkdown from 'react-markdown'
 
 const Content = ({ children }) => {
     return (
       <span className="ml-6 pb-4 leading-normal text-black text-base">
-        {children}
+        <ReactMarkdown>{`${children}`}</ReactMarkdown>
       </span>
     );
   };

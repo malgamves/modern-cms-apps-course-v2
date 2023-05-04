613d0bcc128ac4b007440c1d4fe5d7388d41ff41
malgamves <malgamves@gmail.com>
2023-05-04 10:09:01 +0200

    lesson 7

--- modern-cms-client/app/page.js
+++ modern-cms-client/app/page.js
@@ -1,13 +1,39 @@
 import PageContainer from "@/components/PageContainer";
-import Hello from "@/components/Hello"
+import Main from "@/components/Main";
+import Title from "@/components/Title";
+import Description from "@/components/Description";
 
 export default async function Home() {
+
+  const data = await getPosts();
  
   return (
     <PageContainer>
-      <h1 className="text-3xl font-bold underline"> 
-        Hello World
-      </h1>
+      <Main>
+        <Title>
+          words from others (for me)
+        </Title>
+        <Description>
+          a recipe for disaster
+        </Description>
+
+        {data.data.map((post) => (
+          <div key={post.attributes.slug}>
+            {post.attributes.title}
+          </div>
+        ))}
+      </Main>
     </PageContainer>
   );
+}
+
+
+async function getPosts() {
+  const res = await fetch(`
+  ${process.env.API_URL}/api/posts`);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data from API");
+  }
+  return res.json();
 }

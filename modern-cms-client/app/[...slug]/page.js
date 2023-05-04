9c3c8a1964ce22905bb772e8781b5c77cddeb35b
malgamves <malgamves@gmail.com>
2023-05-04 11:21:04 +0200

    lesson 10

--- modern-cms-client/app/[...slug]/page.js
+++ modern-cms-client/app/[...slug]/page.js
@@ -1,21 +1,47 @@
-import PageContainer from "@/components/PageContainer";
-import Main from "@/components/Main";
-import Title from "@/components/Title";
 import Description from "@/components/Description";
+import LeftGrid from "@/components/LeftGrid";
+import Centered from "@/components/Centered";
+import Title from "@/components/Title";
+import Back from "@/components/Back";
+import RightGrid from "@/components/RightGrid";
+import Content from "@/components/Content";
+import PageContainer from "@/components/PageContainer";
 
+import Link from "next/link"
 export default async function Blog({ params }) {
 
   const { slug } = params;
   const data = await getPage(slug);
   const posts = data.data[0];
 
+
+  function reformatDate(fullDate) {
+    const date = new Date(fullDate);
+    return date.toDateString().slice(4); 
+  }
+
   return (
     <PageContainer>
-      <Main>
-        <Title>
-          {posts.attributes.title}
-        </Title>
-      </Main>
+      <LeftGrid>
+        <Centered>
+          <Title>
+            {posts.attributes.title}
+          </Title>
+          <Description className="text-black text-base font-semibold"> by <span className="text-red-500"> {posts.attributes.author} </span> on { reformatDate(posts.attributes.date)}</Description>
+
+          <Back>
+            <Link href="/"> &larr; go back home</Link>
+          </Back>
+
+        </Centered>
+        </LeftGrid>
+        <RightGrid>
+          <Centered>
+            <Content>
+              {posts.attributes.body}
+            </Content>
+          </Centered>
+        </RightGrid>
     </PageContainer>
   )
 }

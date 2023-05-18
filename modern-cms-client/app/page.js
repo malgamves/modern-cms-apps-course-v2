186c3c986a15ffb235e778552de808ec66f16f34
malgamves <malgamves@gmail.com>
2023-05-18 09:04:48 +0200

    course commits

--- modern-cms-client/app/page.js
+++ modern-cms-client/app/page.js
@@ -2,41 +2,38 @@ import PageContainer from "@/components/PageContainer";
 import Main from "@/components/Main";
 import Title from "@/components/Title";
 import Description from "@/components/Description";
-import List from "@/components/List";
-import Pagination from "@/components/Pagination";
 
+export default async function Home() {
 
+  const data = await getPosts();
 
-export default async function Home({ searchParams }) {
-
-  const { page } = searchParams;
-  const pageParam = page === undefined ? 1 : page;
-  const data = await getPosts(pageParam);
- 
   return (
     <PageContainer>
       <Main>
         <Title>
           words from others (for me)
         </Title>
-        <Description>
-          a recipe for disaster
+        <Description> 
+          a recipe for disaster 
         </Description>
-        <List allPosts={data} />
-        <Pagination paginationData={data.meta.pagination} />
-       
+				
+        {data.data.map((post) => (
+          <div key={post.attributes.id}>
+            {post.attributes.title}
+          </div>
+        ))}
       </Main>
     </PageContainer>
   );
 }
 
 
-async function getPosts(pageParam) {
+async function getPosts() {
   const res = await fetch(`
-  ${process.env.API_URL}/api/posts?pagination[page]=${pageParam}&pagination[pageSize]=3`);
+  ${process.env.API_URL}/api/posts`);
 
   if (!res.ok) {
-    throw new Error("Failed to fetch data from API");
+    throw new Error("Failed to fetch data from API")
   }
   return res.json();
 }
